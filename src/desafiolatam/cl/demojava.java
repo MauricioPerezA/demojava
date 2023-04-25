c74bd3fae1ae2f8d5f36bcbd1ccbd00294810f53
Mauroko <m.perez.arjona@gmail.com>
2023-04-24 22:39:10 -0400

    demojava

---
+++ src/desafiolatam/cl/demojava.java
@@ -0,0 +1,17 @@
+/*
+ * Primer ejemplo de java demojava
+ */
+package desafiolatam.cl;
+
+public class demojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Hola");
+		System.out.println("Hola otra vez");
+		System.out.println("Hoooooola");
+		}
+
+	}
+
+
